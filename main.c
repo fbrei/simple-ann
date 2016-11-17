90009932295349a5f1bc3c41a3c6884cce3a74f0
Felix Brei <felix.brei@posteo.de>
2016-11-17 22:44:37 +0100

    Made the example cooler

--- main.c
+++ main.c
@@ -21,6 +21,8 @@
 // START
 
 int min(int x, int y) { return (x < y) ? x : y; }
+int max(int x, int y) { return (x < y) ? y : x; }
+double delta(double x, double y) { return fabs(x-y); }
 
 int main(int argc, char** argv) {
 
@@ -30,44 +32,58 @@ int main(int argc, char** argv) {
 	init_activation_functions();
 
 	// Configuration variables
-	const int NUM_HIDDEN_LAYERS = 6;
-	const int NUM_NEURONS_PER_HIDDEN_LAYER = 256;
-	ActFunction* ACT_FUNCTION = ID;
+	const int NUM_HIDDEN_LAYERS = 4;
+	const int NUM_NEURONS_PER_HIDDEN_LAYER = 32;
+	ActFunction* ACT_FUNCTION = SIGMOID;
 
 	const int NUM_INPUTS = 2;
 	const int NUM_OUTPUTS = 3;
 	
-	const int MAX_NUM_TRAINING_ROUNDS = 10000;
-	double BACKPROP_STEP_SIZE = 0.0000000001;
-	double MAX_ERROR = 0.01;
+	const int MAX_NUM_TRAINING_ROUNDS = 500000;
+	double BACKPROP_STEP_SIZE = 1;
+	double MAX_ERROR = 10.0;
+	double MIN_ERROR_DELTA = 0.000001;
 
-	const int MAP_HEIGHT = 100;
-	const int MAP_WIDTH = 100;
+	const int MAP_WIDTH = 1600;
+	const int MAP_HEIGHT = 900;
 
 	// Suppose we want to learn the RGB components of a pixel of an image
-	double* target = malloc( 3 * sizeof(double));
-	target[0] = 20.0;
-	target[1] = 10.0;
-	target[2] = 200.0;
+	double* first_target = malloc( 3 * sizeof(double));
+	first_target[0] = 220.0;
+	first_target[1] = 10.0;
+	first_target[2] = 1.0;
 
-	double* pixel = malloc( 2 * sizeof(double) );
-	pixel[0] =  25;
-	pixel[1] =  25;
+	double* first_pixel = malloc( 2 * sizeof(double) );
+	first_pixel[0] =  1;
+	first_pixel[1] =  1;
 
-	BACKPROP_STEP_SIZE /= (pixel[0] * pixel[1]);
+	double* second_target = malloc( 3 * sizeof(double));
+	second_target[0] = 15.0;
+	second_target[1] = 1.0;
+	second_target[2] = 221.0;
+
+	double* second_pixel = malloc( 2 * sizeof(double) );
+	second_pixel[0] =  1600;
+	second_pixel[1] =  900;
+
+	BACKPROP_STEP_SIZE /= (second_pixel[0] * second_pixel[1] * NUM_NEURONS_PER_HIDDEN_LAYER * NUM_HIDDEN_LAYERS);
 
 
 	// Allocate and connect everything ... and I mean, everything ;)
-	NeuronLayer* in = create_input(NUM_INPUTS,1, ACT_FUNCTION);
-	NeuronLayer* out = create_output(NUM_OUTPUTS, ACT_FUNCTION);
+	NeuronLayer* in = create_input(NUM_INPUTS,1, ID);
+	NeuronLayer* out = create_output(NUM_OUTPUTS, ID);
 
 	NeuronLayer** hidden = malloc( NUM_HIDDEN_LAYERS * sizeof(NeuronLayer*) );
 	
 	hidden[0] = alloc_neuron_layer(NUM_NEURONS_PER_HIDDEN_LAYER, ACT_FUNCTION);
 	connect_layers(in, hidden[0]);
+	connect_layers(hidden[0],out);
+	connect_layers(in, out);
 
 	for(int i = 1; i < NUM_HIDDEN_LAYERS; i++) {
 		hidden[i] = alloc_neuron_layer(NUM_NEURONS_PER_HIDDEN_LAYER, ACT_FUNCTION);
+		connect_layers(in, hidden[i]);
+		connect_layers(hidden[i], out);
 		for(int j = 0; j < i; j++) {
 			connect_layers(hidden[j], hidden[i]);
 		}
@@ -79,73 +95,113 @@ int main(int argc, char** argv) {
 	// ===========================================================================
 	// Insert magic here
 
-	set_input(in, pixel);
+	double* error_vector = malloc(NUM_OUTPUTS * sizeof(double));
+
+	double total_error = 9999.9;
+	double old_error = 0.0;
+	int round = 1;
+
+	double* res;
+	double local_backprop_stepsize = 0.0;
+	double scaling = 0.0;
+	while(total_error > MAX_ERROR && round < MAX_NUM_TRAINING_ROUNDS) {
+
+		scaling = 1.0 / fabs(delta(total_error, old_error)),10.0;
+		scaling = (scaling > 100.0) ? 100.0 : scaling;
+		local_backprop_stepsize = BACKPROP_STEP_SIZE * scaling;
+		old_error = total_error;
+
+		// Train first pixel
+		set_input(in, first_pixel);
+
+		fire_all(in);
+		for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
+			fire_all(hidden[i]);
+		}
+		fire_all(out);
+			
+		res = get_output(out);
+		error_vector[0] = first_target[0] - res[0];
+		error_vector[1] = first_target[1] - res[1];
+		error_vector[2] = first_target[2] - res[2];
+
+		total_error = fabs(error_vector[0]) + fabs(error_vector[1]) + fabs(error_vector[2]);
+
+		set_initial_gradient(out, error_vector);
+		backprop_all(out, local_backprop_stepsize);
+		for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
+			backprop_all(hidden[i], local_backprop_stepsize);
+		}
+		backprop_all(in, local_backprop_stepsize);
+
+		// Train second pixel
+		set_input(in, second_pixel);
+
+		fire_all(in);
+		for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
+			fire_all(hidden[i]);
+		}
+		fire_all(out);
+			
+		res = get_output(out);
+		error_vector[0] = second_target[0] - res[0];
+		error_vector[1] = second_target[1] - res[1];
+		error_vector[2] = second_target[2] - res[2];
+
+		total_error += fabs(second_target[0] - res[0]) + fabs(second_target[1] - res[1]) + fabs(second_target[2] - res[2]);
+
+		set_initial_gradient(out, error_vector);
+		backprop_all(out, local_backprop_stepsize);
+		for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
+			backprop_all(hidden[i], local_backprop_stepsize);
+		}
+		backprop_all(in, local_backprop_stepsize);
+
+
+		if(round % 500 == 0) {
+			printf("%d | %d : ERROR:  %f \t %f \t %f \t Total: %f \t Backprop scaling %f \n", round, MAX_NUM_TRAINING_ROUNDS, fabs(error_vector[0]), fabs(error_vector[1]), fabs(error_vector[2]), total_error, scaling);
+		}
+		free(res);
+
+		//if(delta(old_error, total_error) < MIN_ERROR_DELTA) { break; }
+		round++;
+	}
+
+
+	set_input(in, first_pixel);
 
 	fire_all(in);
 	for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
 		fire_all(hidden[i]);
 	}
 	fire_all(out);
-
-	double* res = get_output(out);
-
-	printf("The network fired: %f | %f | %f \n", res[0], res[1], res[2]);
-	free(res);
-
-	double* error_vector = malloc(NUM_OUTPUTS * sizeof(double));
-
-//	for(int i = 0; i < NUM_TRAINING_ROUNDS; i++) {
-
-	double total_error = 9999.9;
-	int round = 1;
-
-	while(total_error > MAX_ERROR && round < MAX_NUM_TRAINING_ROUNDS) {
-			
-		res = get_output(out);
-		error_vector[0] = target[0] - res[0];
-		error_vector[1] = target[1] - res[1];
-		error_vector[2] = target[2] - res[2];
-
-		total_error = fabs(error_vector[0]) + fabs(error_vector[1]) + fabs(error_vector[2]);
-
-		printf("%d | %d : ERROR:  %f \t %f \t %f\n", round, MAX_NUM_TRAINING_ROUNDS, fabs(error_vector[0]), fabs(error_vector[1]), fabs(error_vector[2]));
-		free(res);
-
-		set_initial_gradient(out, error_vector);
-		backprop_all(out, BACKPROP_STEP_SIZE);
-		for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
-			backprop_all(hidden[i], BACKPROP_STEP_SIZE);
-		}
-		backprop_all(in, BACKPROP_STEP_SIZE);
-	
-		fire_all(in);
-		for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
-			fire_all(hidden[i]);
-		}
-		fire_all(out);
-	
-		round++;
-	}
-
-
+		
 	res = get_output(out);
+	printf("The network fired: %f | %f \t %f | %f \t %f | %f \n", first_target[0], res[0], first_target[1], res[1], first_target[2], res[2]);
 
-	printf("The network fired: %f | %f | %f \n", res[0], res[1], res[2]);
+	set_input(in, second_pixel);
 
-
-
+	fire_all(in);
+	for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
+		fire_all(hidden[i]);
+	}
+	fire_all(out);
+		
+	res = get_output(out);
+	printf("The network fired: %f | %f \t %f | %f \t %f | %f \n", second_target[0], res[0], second_target[1], res[1], second_target[2], res[2]);
 
 	// Write the perceptive map to a file
 	
 	FILE *fp;
 	fp = fopen("perceptive_map.ppm", "w");
 
-	fprintf(fp, "P3\n%d %d\n255\n", MAP_HEIGHT, MAP_WIDTH);
+	fprintf(fp, "P3\n%d %d\n255\n", MAP_WIDTH, MAP_HEIGHT);
 	for(int i = 1; i <= MAP_WIDTH; i++) {
+		if(i % 20 == 0 ) printf("Calculating (%d,x)\n",i);
 		for(int j = 0; j <= MAP_HEIGHT; j++) {
-			pixel[0] = i;
-			pixel[1] = j;
-			set_input(in, pixel);
+			first_pixel[0] = i;
+			first_pixel[1] = j;
+			set_input(in, first_pixel);
 			fire_all(in);
 			for(int i = 0; i < NUM_HIDDEN_LAYERS; i++) {
 				fire_all(hidden[i]);
@@ -154,14 +210,14 @@ int main(int argc, char** argv) {
 		
 			res = get_output(out);
 
-			printf("Calculating (%d,%d)\n",i,j);
-			fprintf(fp, "%d %d %d ", min((int)res[0],255), min((int)res[1],255), min((int)res[2],255) );
+			fprintf(fp, "%d %d %d ", min(max((int)res[0],0),255), min(max((int)res[1],0),255), min(max((int)res[2],0),255) );
 		}
 		fprintf(fp,"\n");
 	}
 
-
 	fclose(fp);
+
+	printf("Done!\n");
 
 	// End of magic
 	// ===========================================================================
@@ -176,9 +232,11 @@ int main(int argc, char** argv) {
 	free(hidden);
 	free_neuron_layer(in);
 	free_neuron_layer(out);
-	free(pixel);
+	free(first_pixel);
+	free(second_pixel);
 	free(res);
-	free(target);
+	free(first_target);
+	free(second_target);
 
 	free_activation_functions();
 
